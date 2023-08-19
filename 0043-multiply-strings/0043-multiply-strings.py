b10158ea6958bd730f2324ef7420dc93400e0b25
saira Arif <115411658+saira1234567@users.noreply.github.com>
2023-08-19 18:41:10 +0500

    Time: 37 ms (93.40%), Space: 16.2 MB (95.07%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,5 +1,6 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+        
         def conversion(num):
             dic={'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
             ans=0
