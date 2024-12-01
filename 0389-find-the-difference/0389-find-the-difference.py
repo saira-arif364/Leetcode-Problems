03a07169297bac45b0b2cd92e88f2a9d179944b7
saira Arif <115411658+saira-arif364@users.noreply.github.com>
2024-12-01 22:11:06 +0500

    Time: 0 ms (100%), Space: 17.4 MB (5.09%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,8 +1,8 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        for n in t:
-            if n in s:
-                s=s.replace(n,'',1)
-            else:
-                return n
+       
+        s_sum = sum(ord(x) for x in s)
+        t_sum = sum(ord(y) for y in t)
+    
+        return chr(t_sum - s_sum)
         
